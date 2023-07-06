77f99acfc9466ceafe55b2b4dc67b04b24665c4e
Ernest Leung <ernestleung52@gmail.com>
2023-07-05 18:41:31 -0700

    Implemented auth0 login/logout and user info display skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
 import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Image from 'next/image';
 
 type Props = {};
 
 const Home = (props: Props) => {
+	const { user } = useUser();
+	console.log(user);
+
 	return (
 		<div>
 			<h1>Next.js + MongoDB</h1>
 			<div>
-				<Link href='/api/auth/login'>Login</Link>
+				{!!user ? (
+					<>
+						<div>
+							<Image src={user.picture} alt={user.name} height={50} width={50} />
+						</div>
+						<div>{user.email}</div>
+						<Link href='/api/auth/logout'>Logout</Link>
+					</>
+				) : (
+					<Link href='/api/auth/login'>Login</Link>
+				)}
 			</div>
 		</div>
 	);
