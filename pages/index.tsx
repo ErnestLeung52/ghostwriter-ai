ad477f0448e2f0edc365bed5be918f1e7a87d807
Ernest Leung <ernestleung52@gmail.com>
2023-07-07 16:08:20 -0700

    Styled landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
-import Link from 'next/link';
-import { useUser } from '@auth0/nextjs-auth0/client';
 import Image from 'next/image';
+import HeroImage from '../public/hero.webp';
+import { Logo } from '../components/Logo';
+import Link from 'next/link';
 
 type Props = {};
 
 const Home = (props: Props) => {
 	return (
 		<div>
-			<h1>Next.js + MongoDB</h1>
-			<div></div>
+			<div className='w-screen h-screen overflow-hidden flex justify-center items-center relative'>
+				<Image src={HeroImage} alt='Hero' fill className='absolute' />
+				<div className='relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm'>
+					<Logo />
+
+					<p>This is an awesome app</p>
+
+					<Link href='/post/new' className='btn'>
+						Begin
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
