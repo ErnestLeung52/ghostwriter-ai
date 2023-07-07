9043341901621584181e396c4e8ed8054704eb5d
Ernest Leung <ernestleung52@gmail.com>
2023-07-07 15:33:52 -0700

    Configured google font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,31 @@
-import { UserProvider } from '@auth0/nextjs-auth0/client';
 import '../styles/globals.css';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { DM_Sans, DM_Serif_Display } from '@next/font/google';
+
+const dmSans = DM_Sans({
+	weight: ['400', '500', '700'],
+	subsets: ['latin'],
+	variable: '--font-dm-sans',
+});
+
+const dmSerifDisplay = DM_Sans({
+	weight: ['400'],
+	subsets: ['latin'],
+	variable: '--font-dm-serif',
+});
 
 function MyApp({ Component, pageProps }) {
 	// If a particular page doesn't have a get layout function associated with it, we just simply returning the page itself.
 	// console.log('myapp', 1);
 	const getLayout = Component.getLayout || ((page: JSX.Element) => page);
 
-	return <UserProvider>{getLayout(<Component {...pageProps} />, pageProps)}</UserProvider>;
+	return (
+		<UserProvider>
+			<main className={`${dmSans.variable} ${dmSerifDisplay.variable} font-body`}>
+				{getLayout(<Component {...pageProps} />, pageProps)}
+			</main>
+		</UserProvider>
+	);
 }
 
 export default MyApp;
