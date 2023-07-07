c48f2a2583ab74e014a43752e0b8701cabb9a776
Ernest Leung <ernestleung52@gmail.com>
2023-07-07 15:17:39 -0700

    Added getLayout function

--- pages/token-topup.tsx
+++ pages/token-topup.tsx
@@ -1,13 +1,23 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { GetServerSideProps } from 'next';
+import AppLayout from '../components/AppLayout/AppLayout';
 
 type Props = {};
+type PageProps = {
+	children: React.ReactNode;
+};
 
 const TokenTopup = (props: Props) => {
 	return <div>token-topup</div>;
 };
 
 export default TokenTopup;
+
+// Only pages that have a getLayout function defined will be wrapped with the layout
+TokenTopup.getLayout = function getLayout(page: JSX.Element, pageProps: PageProps) {
+	// console.log('new', 3);
+	return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
 
 export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
 	getServerSideProps: async (ctx) => {
