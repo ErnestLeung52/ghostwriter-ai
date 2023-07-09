f81480332f1364ab59f36631749001ed11097ced
Ernest Leung <ernestleung52@gmail.com>
2023-07-08 18:36:06 -0700

    Set up OpenAI API postGen function with text-davinci-003

---
+++ pages/api/generatePost.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, OpenAIApi } from 'openai';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const config = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+
+	const openai = new OpenAIApi(config);
+
+	const topic = 'Top 10 tips for dog owners';
+	const keywords = 'dog health issues, dog breeds, dog care';
+
+	const response = await openai.createCompletion({
+		model: 'text-davinci-003',
+		// risky level of how much it will deviate from the prompt
+		temperature: 0,
+		max_tokens: 3600,
+		prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}.
+    The content should be formatted in SEO-friendly HTML.
+    The response must also include appropriate HTML title and meta description content.
+    The return format must be stringified JSON in the following format:
+    {
+      "postContent": post content,
+      "title": title content,
+      "metaDescription": meta description content
+    }
+    `,
+	});
+
+	console.log(response.data);
+
+	res.status(200).json({ post: JSON.parse(response.data.choices[0]?.text.split('\n').join('')) });
+}
