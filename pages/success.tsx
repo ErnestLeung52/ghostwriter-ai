ee5da100de647511a39fd0a89ed1bac2701988f0
Ernest Leung <ernestleung52@gmail.com>
2023-07-26 15:04:46 -0700

    Installed react-confetti for display animation in success page

--- pages/success.tsx
+++ pages/success.tsx
@@ -3,13 +3,25 @@ import { GetServerSideProps } from 'next';
 import AppLayout from '../components/AppLayout/AppLayout';
 import { PageProps } from '../types';
 import { getAppProps } from '../utils/getAppProps';
+import Confetti from 'react-confetti';
+import { useEffect, useRef, useState } from 'react';
 
 type Props = {};
 
 const Success = (props: Props) => {
+	const [height, setHeight] = useState(null);
+	const [width, setWidth] = useState(null);
+
+	useEffect(() => {
+		setHeight(window.innerHeight);
+		setWidth(window.innerWidth);
+	}, []);
+
 	return (
-		<div>
-			<h1>Thank you for your purchase!</h1>
+		<div className='flex flex-col justify-center items-center bg-slate-900'>
+			<h1 className='text-white'>Thank you for your purchase!</h1>
+			<p className='text-slate-500 text-xs'>Although it is a mocked payment, I still appreciate it.</p>
+			<Confetti numberOfPieces={80} width={width} height={height} />
 		</div>
 	);
 };
